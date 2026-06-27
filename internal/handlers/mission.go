f01be18500bfdaaefa64b9f650ee0c0573cccacc
Lucas Da Cruz <lucaspavesi11@gmail.com>
2026-06-27 16:39:22 -0400

    finish mission handler

--- internal/handlers/mission.go
+++ internal/handlers/mission.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"garrison/internal/models"
 	"garrison/internal/stores"
 	"net/http"
 
@@ -19,7 +20,6 @@ func NewMissionHandler(store stores.MissionStore) *MissionHandler {
 }
 
 func (h *MissionHandler) GetMissionByID(w http.ResponseWriter, r *http.Request) {
-
 	id := r.PathValue("id")
 
 	if id == "" {
@@ -42,4 +42,71 @@ func (h *MissionHandler) GetMissionByID(w http.ResponseWriter, r *http.Request) 
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(mission)
+}
+
+func (h *MissionHandler) GetAllMissions(w http.ResponseWriter, r *http.Request) {
+	missions, err := h.store.GetAll(r.Context())
+
+	if err != nil {
+		http.Error(w, "error retrieving missions", http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(missions)
+}
+
+func (h *MissionHandler) CreateMission(w http.ResponseWriter, r *http.Request) {
+	var m models.Mission
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	newMission, err := h.store.Create(r.Context(), &m)
+	if err != nil {
+		http.Error(w, "error in creating new mission", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newMission)
+}
+
+func (h *MissionHandler) DeleteMission(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	if id == "" {
+		http.Error(w, "id cannot be empty", http.StatusBadRequest)
+	}
+
+	parsed_id, err := uuid.Parse(id)
+
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	err = h.store.Delete(r.Context(), parsed_id)
+	if err != nil {
+		http.Error(w, "error deleting mission", http.StatusBadRequest)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *MissionHandler) UpdateMission(w http.ResponseWriter, r *http.Request) {
+	var m models.Mission
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	updatedMission, err := h.store.Update(r.Context(), &m)
+	if err != nil {
+		http.Error(w, "error in creating new mission", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedMission)
 }
